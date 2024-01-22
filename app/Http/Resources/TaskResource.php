442a60324ba2f6126207c6c826ebb4be0142273d
Muhammad Rizki <rizkielshirazy@gmail.com>
2024-01-22 11:08:36 +0700

    create & show task with validation

---
+++ app/Http/Resources/TaskResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TaskResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        $data = parent::toArray($request);
+        $data['status'] = $this->is_done ? 'finished' : 'open';
+
+        return $data;
+    }
+}
