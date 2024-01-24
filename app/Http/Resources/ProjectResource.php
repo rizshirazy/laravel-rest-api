8f18a76df145adafa3bf5e35fb448725dd207de0
Muhammad Rizki <rizkielshirazy@gmail.com>
2024-01-24 10:22:29 +0700

    fetch the project list

--- app/Http/Resources/ProjectResource.php
+++ app/Http/Resources/ProjectResource.php
@@ -17,7 +17,7 @@ class ProjectResource extends JsonResource
         return [
             'id'         => $this->id,
             'title'      => $this->title,
-            'task'       => TaskResource::collection($this->tasks),
+            'task'       => TaskResource::collection($this->whenLoaded('tasks')),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
